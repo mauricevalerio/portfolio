fa6602dcb4a8018d869cba794d23d5ae62eb9260
mauricevalerio <75749759+mauricevalerio@users.noreply.github.com>
2023-04-01 23:55:15 -0700

    Added scrollbar design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ export default function Hero() {
     return (
         <section id='hero' className='component-layout primary-color'>
             <div className='component-child-larger title'>
-                <h1>&#60;Maurice Valerio&#62;</h1>
+                <h1 className='name'>&#60;Maurice Valerio&#62;</h1>
                 <h1 className='job-title'>Front End Web Developer</h1>
             </div>
 
