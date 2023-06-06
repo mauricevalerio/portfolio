66d0758c8c2eb0c86b72fb4d4572afa629dcc24a
mauricevalerio <75749759+mauricevalerio@users.noreply.github.com>
2023-06-06 01:49:40 -0700

    Update sendEmail.js

--- src/sendEmail.js
+++ src/sendEmail.js
@@ -1,7 +1,6 @@
 import emailjs from '@emailjs/browser'
 
 export default function sendEmail(setIsLoading, setIsFormSubmitted, setIsSendingFailed, formData) {
-    // emailjs.send('service_9oculcp', 'template_fmd1hsw', formData, 'SiBnDjVTgqD9VQ93b')
     emailjs.send(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, formData, import.meta.env.VITE_PUBLIC_KEY)
         .then((response) => {
             if (response.status === 200) {
