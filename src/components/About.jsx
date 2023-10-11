f64b61f8f9938034205004505802536506036ad5
mauricevalerio <75749759+mauricevalerio@users.noreply.github.com>
2023-10-11 01:25:59 -0700

    Modified about me contents

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,9 @@ export default function About() {
     return (
         <section className='about'>
             <p>
-                Currently transitioning from front end development to full stack development.
-                I specialize in HTML, CSS, JavaScript, TypeScript, and React. I love keeping my brain active by building web applications because it also helps me deal with challenges I face in everyday life by thinking smart leading to efficient solutions.
+                Proficient in HTML, CSS, JavaScript, and TypeScript, and always up to date with latest front-end libraries and frameworks.
+                I apply best practices in responsive layouts, UI design, enhancing user interactivity for best possible accessibility.
+                I've got basic understanding on back-end architecture ranging from web server technologies to databases to create APIs and ensure data integrity.
                 Currently active on <a href="https://scrimba.com/learn/frontend" target='_blank' className='hover-effect'>Scrimba</a> and always trying out new technologies and implementing it on <a href="https://www.frontendmentor.io/challenges" target='_blank' className='hover-effect'>Frontendmentor challenges</a>.
                 I've also used the following technologies listed below.
             </p>
