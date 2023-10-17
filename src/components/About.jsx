d468f674727da7722b8ab1f8cfd7df9671e8e1c7
mauricevalerio <75749759+mauricevalerio@users.noreply.github.com>
2023-10-17 09:27:46 -0700

    Modified about me contents

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,11 +2,9 @@ export default function About() {
     return (
         <section className='about'>
             <p>
-                Proficient in HTML, CSS, JavaScript, and TypeScript, and always up to date with latest front-end libraries and frameworks.
-                I apply best practices in responsive layouts, UI design, enhancing user interactivity for best possible accessibility.
-                I've got basic understanding on back-end architecture ranging from web server technologies to databases to create APIs and ensure data integrity.
+                Proficient in HTML, CSS, JavaScript, and TypeScript. Keen attention to detail on layouts, responsive design, and UI design. Enhances visual design and user interactivity for best possible accessibility.
+                I have basic understanding on back-end architecture ranging from web server technologies to databases to create web APIs and ensure data integrity.
                 Currently active on <a href="https://scrimba.com/learn/frontend" target='_blank' className='hover-effect'>Scrimba</a> and always trying out new technologies and implementing it on <a href="https://www.frontendmentor.io/challenges" target='_blank' className='hover-effect'>Frontendmentor challenges</a>.
-                I've also used the following technologies listed below.
             </p>
         </section>
     )
