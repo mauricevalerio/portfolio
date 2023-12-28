8d741ad73422b2cda67823790d3cb957d09a3419
mauricevalerio <75749759+mauricevalerio@users.noreply.github.com>
2023-12-28 01:05:36 -0800

    Modified about me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,8 @@ export default function About() {
     return (
         <section className='about'>
             <p>
-                Proficient in HTML, CSS, JavaScript, and TypeScript. Keen attention to detail on layouts, responsive design, and UI design. Enhances visual design and user interactivity for accessibility.
-                I have basic understanding on back-end architecture ranging from web server technologies to databases to create web APIs and ensure data integrity.
-                Doing <a href="https://www.frontendmentor.io/challenges" target='_blank' className='hover-effect'>Frontendmentor challenges</a> for fun.
+                Proficient in HTML, CSS, JavaScript, and TypeScript. I find it fun creating interactive and engaging web experiences without compromising accessibility.
+                Currently doing <a href="https://www.frontendmentor.io/challenges" target='_blank' className='hover-effect'>Frontendmentor challenges</a>.
             </p>
         </section>
     )
