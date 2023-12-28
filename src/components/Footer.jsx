c027fee28e9fe3239764223802121f21050169e1
mauricevalerio <75749759+mauricevalerio@users.noreply.github.com>
2023-12-28 01:06:18 -0800

    Modified footer contents

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,6 @@
 export default function Footer() {
     return (
         <footer className='footer'>
-            <p>Logo and graphic design by <a href='https://www.instagram.com/artsyetph/' target='_blank' className='hover-effect'>artsyetph</a></p>
             <p>Maurice Valerio © 2023</p>
         </footer>
     )
